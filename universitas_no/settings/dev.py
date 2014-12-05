408dcec1c057bde112a5db1d71cc0d9707c84e5e
Håken Lid <haakenlid@gmail.com>
2014-12-05 20:21:15 +0100

    domeneshop db

--- universitas_no/settings/dev.py
+++ universitas_no/settings/dev.py
@@ -17,7 +17,6 @@ EMAIL_HOST_USER = environ["DJANGO_DEV_GMAIL_USER"] + '@gmail.com'
 EMAIL_HOST_PASSWORD = environ["DJANGO_DEV_GMAIL_PASSWORD"]
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
 # END EMAIL CONFIGURATION
 INSTALLED_APPS += (
     'debug_toolbar',
@@ -31,3 +30,23 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
 # http://django-debug-toolbar.readthedocs.org/en/latest/installation.html
 INTERNAL_IPS = ('84.211.75.214')
+
+DATABASES['prodsys']['HOST'] = 'localhost'
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': environ["DJANGO_DB_NAME"],
+#         'USER': environ["DJANGO_DB_USER"],
+#         'PASSWORD': environ["DJANGO_DB_PASSWORD"],
+#         'HOST': 'localhost',
+#         'PORT': '',       # Set to empty string for default.
+#     },
+#     'prodsys': {
+#         'ENGINE': 'mysql.connector.django',
+#         'NAME': environ["DJANGO_PRODSYS_DB_NAME"],
+#         'USER': environ["DJANGO_PRODSYS_DB_USER"],
+#         'PASSWORD': environ["DJANGO_PRODSYS_DB_PASSWORD"],
+#         'HOST': environ["DJANGO_PRODSYS_DB_HOST"],
+#         # 'HOST': 'localhost', # TODO: Endre dette tilbake det er bare et eksperiment.
+#         'PORT': '',       # Set to empty string for default.
+#     }
+# }
