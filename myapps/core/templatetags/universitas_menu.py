8d5d5dbe6771cbb9919e4c0dfb10550561506062
Håken Lid <haakenlid@gmail.com>
2015-02-26 23:23:44 +0100

    toppmeny

---
+++ myapps/core/templatetags/universitas_menu.py
@@ -0,0 +1,37 @@
+""" Template tags for inline story elements such as images, asides and pullquotes """
+
+import logging
+from django import template
+from myapps.stories.models import Section, Story
+
+register = template.Library()
+logger = logging.getLogger('universitas')
+
+@register.inclusion_tag('universitas-menu.html')
+def universitas_menu(active_section):
+
+    sections = [
+        Section.objects.get(
+            title__iexact=title) for title in [
+            'nyheter',
+            'kultur',
+            'debatt',
+            'magasin']]
+
+    context = {
+        'sections': sections,
+        'active_section': active_section,
+    }
+    return context
+
+
+@register.inclusion_tag('top-stories.html')
+def top_stories(section, number, order_by):
+    stories = Story.objects.published().filter(
+        story_type__section=section
+    ).order_by(order_by)[:number]
+
+    context = {
+        "stories": stories,
+    }
+    return context
