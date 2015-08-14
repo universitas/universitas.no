35e44459a71e972e4def0d7e6fd0e272366abb38
Håken Lid <haakenlid@gmail.com>
2015-08-14 14:36:59 +0200

    tweak load og dump

--- bin/loaddata.sh
+++ bin/loaddata.sh
@@ -8,4 +8,4 @@ LATEST_DUMP=$(ls -t /srv/*dump*.json | head -n1)
 # dump database in json format
 # django-admin dumpdata --exclude sessions --exclude watson --indent=2 > /srv/"$DJANGO_DB_NAME"_dump_$(date +"%Y-%m-%d").json
 # load database
-echo $LATEST_DUMP
+django-admin loaddata $LATEST_DUMP
