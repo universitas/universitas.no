b20e49cdd16e2b58619946aa92d7c3bcd766a046
Håken Lid <haakenlid@gmail.com>
2015-08-14 14:32:55 +0200

    loaddata

--- bin/loaddata.sh
+++ bin/loaddata.sh
@@ -1,1 +1,11 @@
-ls -t www_universitas_no_dump_2015-08-14.json
+#!/bin/bash
+
+# load environmental variables.
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/../../venv/**/bin/activate
+LATEST_DUMP=$(ls -t /srv/*dump*.json | head -n1)
+
+# dump database in json format
+# django-admin dumpdata --exclude sessions --exclude watson --indent=2 > /srv/"$DJANGO_DB_NAME"_dump_$(date +"%Y-%m-%d").json
+# load database
+echo $LATEST_DUMP
