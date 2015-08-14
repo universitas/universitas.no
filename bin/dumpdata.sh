59f85bc9843026ea639b44d5740a16c05fefd10f
Håken Lid <haakenlid@gmail.com>
2015-08-14 14:24:48 +0200

    load and dump

--- bin/dumpdata.sh
+++ bin/dumpdata.sh
@@ -5,4 +5,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/../../venv/**/bin/activate
 
 # dump database in json format
-django-admin dumpdata --exclude sessions --exlude watson --indent=2 > /srv/"$DJANGO_DB_NAME"_dump_$(date +"%Y-%m-%d").json
+django-admin dumpdata --exclude sessions --exclude watson --indent=2 > /srv/"$DJANGO_DB_NAME"_dump_$(date +"%Y-%m-%d").json
---
+++ bin/loaddata.sh
@@ -0,0 +1,1 @@
+ls -t www_universitas_no_dump_2015-08-14.json
