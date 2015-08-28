b4648b8e16c6a8d8f5e70a646e2fa44574c28831
Håken Lid <haakenlid@gmail.com>
2015-08-28 16:00:48 +0200

    en bot skjønner ikke tel: i href. Redirect i stedet

--- universitas_no/redirect_urls.py
+++ universitas_no/redirect_urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    # PHP utils from old website
-    url(r'^(?P<base>.+)/hl:.+$', RedirectView.as_view(url=r'/%(base)s/')),
+    # PHP utils from old website and other cruft that older browsers might request.
+    url(r'^(?P<base>.+)/(hl|tel):.+$', RedirectView.as_view(url=r'/%(base)s/')),
     url(r'^rss\.php$', RedirectView.as_view(url=r'/rss/')),
     # Retired pages from old website
     url(r'^inc/func/image\.inc\.php', RedirectView.as_view(url=None)),
@@ -13,6 +13,8 @@ urlpatterns = [
     # Rebranded pages
     url(r'^nyhet/$', RedirectView.as_view(url=r'/nyheter/')),
     url(r'^vispor/$', RedirectView.as_view(url=r'/baksiden/vi-spor/')),
-    url(r'^nyheter/omverden/$', RedirectView.as_view(url=r'/nyheter/utenriks/')),
+    url(r'^nyheter/omverden/$',
+        RedirectView.as_view(url=r'/nyheter/utenriks/')),
     url(r'^omverden/$', RedirectView.as_view(url=r'/nyheter/utenriks/')),
+    # Annoying bot doesn't understand href="tel:..."
 ]
