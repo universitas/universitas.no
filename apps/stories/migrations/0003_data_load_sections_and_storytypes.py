fcfa0859aa80ca3dcb8c30fe2864a5e35cbc6f9f
Håken Lid <haakenlid@gmail.com>
2015-09-08 17:23:18 +0200

    fixe fixtures

--- apps/stories/migrations/0003_data_load_sections_and_storytypes.py
+++ apps/stories/migrations/0003_data_load_sections_and_storytypes.py
@@ -1,29 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.db import models, migrations
+from django.db import migrations
+from django.core.management import call_command
 import os
-from django.core import serializers
 
 fixture_dir = os.path.dirname(__file__)
 fixture_filename = 'sections_and_story_types.json'
 
 
 def load_fixture(apps, schema_editor):
-    from django.core.management import call_command
-
+    """Load fixtures for sections and story types"""
     def load_fixture(apps, schema_editor):
         call_command('loaddata', fixture_filename, app_label='stories')
-    # fixture_file = os.path.join(fixture_dir, fixture_filename)
-
-    # fixture = open(fixture_file, 'rb')
-    # objects = serializers.deserialize('json', fixture, ignorenonexistent=True)
-    # for obj in objects:
-    #     obj.save()
-    # fixture.close()
 
 
 def unload_fixture(apps, schema_editor):
-    "Brutally deleting all entries for this model..."
+    """Brutally deleting all entries for this model..."""
     for modelname in ('Section', 'StoryType'):
         model = apps.get_model('stories', modelname)
         model.objects.all().delete()
