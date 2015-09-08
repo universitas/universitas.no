391e0989d87f49ea1ef9936c847cc2257d1395b8
Håken Lid <haakenlid@gmail.com>
2015-09-08 17:13:52 +0200

    clone_db funker

--- bin/clone_db.sh
+++ bin/clone_db.sh
@@ -6,7 +6,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/../../venv/**/bin/activate
 
 sudo supervisorctl stop all
-sudo -u psql -c "DROP DATABASE IF EXISTS $DJANGO_DB_NAME;"
-sudo -u psql -c "CRATE DATABASE $DJANGO_DB_NAME WITH TEMPLATE $ORIGINAL OWNER $DJANGO_DB_USER;"
-sudo -u psql -c ""
+sudo -u postgres psql -c "DROP DATABASE IF EXISTS $DJANGO_DB_NAME;"
+sudo -u postgres psql -c "CREATE DATABASE $DJANGO_DB_NAME WITH TEMPLATE $ORIGINAL OWNER $DJANGO_DB_USER;"
+sudo -u postgres psql -d $DJANGO_DB_NAME -c "REASSIGN OWNED BY $ORIGINAL TO $DJANGO_DB_USER;"
 sudo supervisorctl start all
