a8ee79b14a298842f956f72e0f589e9f6e9005b1
Håken Lid <haakenlid@gmail.com>
2015-10-14 13:03:10 +0200

    celery config

---
+++ django/core/celery.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Configuration for the task runner celery
+"""
+from __future__ import absolute_import
+
+from celery import Celery
+from django.conf import settings
+
+celery_app = Celery('funkytime')
+
+# Using a string here means the worker will not have to
+# pickle the object when using Windows.
+celery_app.config_from_object('django.conf:settings')
+celery_app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+@celery_app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
---
+++ django/core/tasks.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from celery.decorators import periodic_task
+from celery.utils.log import get_task_logger
+from datetime import timedelta
+import subprocess
+
+logger = get_task_logger(__name__)
+
+@periodic_task(run_every=timedelta(hours=1))
+def test_log():
+    """ Logs an interesting message! """
+    fortune = subprocess.check_output(['fortune'])
+    logger.warn("\n%s" % fortune)
