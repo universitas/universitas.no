4db68b260f867b5443c9054661a6b13251e261ff
Håken Lid <haakenlid@gmail.com>
2015-10-14 02:11:10 +0200

    set migration dependency

--- django/apps/contributors/migrations/0010_auto_20151013_2307.py
+++ django/apps/contributors/migrations/0010_auto_20151013_2307.py
@@ -8,6 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('photo', '0004_auto_20151014_0004'),
         ('contributors', '0009_auto_20150924_0005'),
     ]
 
@@ -15,6 +16,12 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='contributor',
             name='byline_photo',
-            field=models.ForeignKey(blank=True, related_name='person', help_text='photo used for byline credit.', to='photo.ProfileImage', on_delete=django.db.models.deletion.SET_NULL, null=True),
+            field=models.ForeignKey(
+                blank=True,
+                related_name='person',
+                help_text='photo used for byline credit.',
+                to='photo.ProfileImage',
+                on_delete=django.db.models.deletion.SET_NULL,
+                null=True),
         ),
     ]
