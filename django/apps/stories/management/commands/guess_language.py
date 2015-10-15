a225a8049301b6898f9fa5286cb3cd4cce77c1de
Håken Lid <haakenlid@gmail.com>
2015-10-15 03:43:21 +0200

    bedre gjetting

--- django/apps/stories/management/commands/guess_language.py
+++ django/apps/stories/management/commands/guess_language.py
@@ -1,15 +1,54 @@
 from optparse import make_option
 import logging
 logger = logging.getLogger(__name__)
+import re
 
 from django.core.management.base import BaseCommand  # , CommandError
 
 from apps.stories.models import Story
 
 LANGUAGE_CORPUS = {
-    'nb': ['en', 'et', 'han', 'hun', 'ikke', 'fra', 'bare', 'noe', ],
-    'nn': ['ein', 'eit', 'han', 'ho', 'ikkje', 'frå', 'berre', 'noko', ],
-    'en': ['a', 'an', 'he', 'she', 'not', 'from', 'only', 'something', ],
+    'nb': [
+        'du',
+        'og',
+        'med',
+        'om',
+        'en',
+        'et',
+        'han',
+        'hun',
+        'ikke',
+        'fra',
+        'bare',
+        'noe',
+    ],
+    'nn': [
+        'du',
+        'og',
+        'med',
+        'om',
+        'ein',
+        'eit',
+        'han',
+        'ho',
+        'ikkje',
+        'frå',
+        'berre',
+        'noko',
+    ],
+    'en': [
+        'you',
+        'and',
+        'with',
+        'a',
+        'an',
+        'he',
+        'she',
+        'not',
+        'from',
+        'only',
+        'something',
+    ],
 }
 
 
@@ -33,7 +72,7 @@ class Command(BaseCommand):
 
         def assign_language_to(story):
             """Guess and assign language to input story"""
-            text = story.get_plaintext()
+            text = story.bodytext_markup
             language = guess_language(text)
             if language != story.language and not dry_run:
                 story.language = language
@@ -48,7 +87,9 @@ class Command(BaseCommand):
         def guess_language(text):
             """Guess language of input text by using a list of common words in candidate languages."""
             score = {key: 0 for key in LANGUAGE_CORPUS.keys()}
-            for word in text.split():
+            words = re.findall(r'[a-zøæå]+', text.lower())
+
+            for word in words:
                 for language, words in LANGUAGE_CORPUS.items():
                     if word in words:
                         score[language] += 1
