5651a7f69410908010a641de13b1950bd7b35734
Håken Lid <haakenlid@gmail.com>
2015-10-15 02:29:19 +0200

    fikse gulp tasks

--- gulpfile.js/tasks/rev/rev-update-references.js
+++ gulpfile.js/tasks/rev/rev-update-references.js
@@ -6,8 +6,7 @@ var revReplace = require('gulp-rev-replace');
 // 2) Update asset references with reved filenames in compiled css + js
 gulp.task('rev-update-references', function(){
   var manifest = gulp.src(path.join(config.root.dest, "rev-manifest.json"));
-
-  return gulp.src(path.join(config.root.dest,'/**/**.{css,js}'))
+  return gulp.src(path.join(config.root.dest,'/**/*.{css,js}'))
     .pipe(revReplace({manifest: manifest}))
     .pipe(gulp.dest(config.root.dest));
 });
