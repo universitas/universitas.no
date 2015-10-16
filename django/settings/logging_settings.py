7feed5ae258d93bd0f21dbd073ed2c6389082d09
Håken Lid <haakenlid@gmail.com>
2015-10-16 19:59:50 +0200

    diverse opprydding i settings

---
+++ django/settings/logging_settings.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""Configurations for logging"""
+from utils.setting_helpers import environment_variable, join_path
+__all__ = ['LOGGING']
+
+LOG_FOLDER = join_path(environment_variable('SOURCE_FOLDER'), '..', 'logs')
+
+
+def logfile_handler(filename, debug=False, **kwargs):
+    config = {
+        'filename': join_path(LOG_FOLDER, filename),
+        'filters': ['require_debug_true'] if debug else ['require_debug_false'],
+        'level': 'DEBUG' if debug else 'WARNING',
+        'class': 'logging.FileHandler',
+        'formatter': 'verbose',
+    }
+    config.update(kwargs)
+    return config
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+}
+LOGGING['filters'] = {
+    'require_debug_false': {
+        '()': 'django.utils.log.RequireDebugFalse'
+    },
+    'require_debug_true': {
+        '()': 'django.utils.log.RequireDebugTrue'
+    }
+}
+LOGGING['formatters'] = {
+    'minimal': {
+        'format': '%(message)s'
+    },
+    'simple': {
+        'format': '%(name)s %(levelname)3s: %(message)s'
+    },
+    'verbose': {
+        'format': (
+            '%(asctime)s [%(levelname)5s] %(name)12s '
+            '%(filename)12s:%(lineno)-4d '
+            '(%(funcName)s)\n\t%(message)s\n'
+        ),
+        'datefmt': '%H:%M:%S %Y-%m-%d'
+    },
+}
+LOGGING['handlers'] = {
+    'errorlog': logfile_handler('error-django.log'),
+    'celerylog': logfile_handler('celery.log'),
+    'debuglog': logfile_handler('debug-django.log', debug=True),
+    'bylineslog': logfile_handler(
+        'bylines.log', level='INFO', filters=[], formatter='minimal',),
+    'sentry': {
+        'level': 'ERROR',
+        'filters': ['require_debug_false'],
+        'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+    },
+    'console': {
+        'filters': ['require_debug_true'],
+        'level': 'DEBUG',
+        'class': 'logging.StreamHandler',
+        'formatter': 'verbose',
+    },
+}
+LOGGING['root'] = {
+    'level': 'WARNING',
+    'handlers': ['console', 'errorlog', 'debuglog', 'sentry'],
+}
+LOGGING['loggers'] = {
+    'bylines': {
+        'level': 'INFO', 'propagate': False,
+        'handlers': ['bylineslog'],
+    },
+    'sorl.thumbnail': {
+        'level': 'WARNING', 'propagate': False,
+        'handlers': ['errorlog', 'debuglog'],
+    },
+    'apps': {
+        'level': 'DEBUG', 'propagate': False,
+        'handlers': ['console', 'errorlog', 'debuglog', 'sentry'],
+    },
+    'core': {
+        'level': 'DEBUG', 'propagate': False,
+        'handlers': ['console', 'errorlog', 'debuglog', 'sentry'],
+    },
+    'celery': {
+        'level': 'INFO', 'propagate': False,
+        'handlers': ['console', 'celerylog', 'sentry'],
+    },
+}
---
+++ django/utils/setting_helpers.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Utility functions for settings"""
+import os
+import logging
+import json
+logger = logging.getLogger(__name__)
+
+
+def environment_variable(keyname):
+    """Shortcut for getting environmental variables"""
+    # To avoid commiting passwords and usernames to git and GitHub,
+    # saved as environmental variables in a file called postactivate.
+    # Postactivate is sourced when the virtual environment is activated.
+    if keyname.startswith('DJANGO_'):
+        keyname = keyname[7:]
+    keyname = 'DJANGO_{}'.format(keyname.upper().replace(' ', '_'))
+    return os.environ.get(keyname) or ''
+
+
+def join_path(*paths):
+    """Shortcut for joining paths. cross os compatible"""
+    return os.path.normpath(os.path.join(*paths))
+
+
+def load_json_file(file_path):
+    """Read a json file and return as a dictionary"""
+    try:
+        with open(file_path) as jsonfile:
+            jsondict = json.load(jsonfile)
+    except IOError:
+        # No file revisions found, continuing without
+        logger.exception('Could not load json file %s' % file_path)
+        jsondict = {}
+
+    return jsondict
