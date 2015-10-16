693b435ed35bf4dd4942917df68d99bcaf2f84fc
Håken Lid <haakenlid@gmail.com>
2015-10-16 20:34:11 +0200

    celery fixes

--- django/apps/core/tasks.py
+++ django/apps/core/tasks.py
@@ -6,8 +6,9 @@ import subprocess
 
 logger = get_task_logger(__name__)
 
-@periodic_task(run_every=timedelta(hours=1))
+# @periodic_task(run_every=timedelta(seconds=10))
+@periodic_task(run_every=timedelta(hours=6))
 def test_log():
     """ Logs an interesting message! """
-    fortune = subprocess.check_output(['fortune'])
-    logger.warn("\n%s" % fortune)
+    fortune = subprocess.check_output(['fortune']).decode('utf-8')
+    logger.info("\n%s" % fortune)
