4421ff1f1e712cda43922454377a6cefc4656b7b
Håken Lid <haakenlid@gmail.com>
2015-10-21 02:19:03 +0200

    web-pdf hotfix

--- django/apps/issues/management/commands/web_pdf.py
+++ django/apps/issues/management/commands/web_pdf.py
@@ -3,17 +3,18 @@ Find PDF files.
 """
 
 from optparse import make_option
-from django.core.management.base import BaseCommand  # ,CommandError
-from django.conf import settings
-
 import os
+import logging
 from datetime import datetime
 import subprocess
 # import re
 from glob import glob
-from apps.issues.models import PrintIssue, current_issue
 
-import logging
+from django.core.management.base import BaseCommand  # ,CommandError
+from django.conf import settings
+from django.core.files.base import ContentFile
+
+from apps.issues.models import PrintIssue, current_issue
 logger = logging.getLogger(__name__)
 
 PDF_STAGING = os.path.join(settings.STAGING_ROOT, 'STAGING', 'PDF')
@@ -21,7 +22,7 @@ PDF_FOLDER = os.path.join(settings.STAGING_ROOT, 'pdf')
 
 PDF_MERGE = os.path.join(settings.PROJECT_DIR, 'bin', 'pdf_merge.sh')
 
-FILENAME_PATTERN = 'universitas_{year}-{number}{suffix}.pdf'
+FILENAME_PATTERN = 'universitas_{issue.date.year}-{issue.number}{suffix}.pdf'
 
 
 class Command(BaseCommand):
@@ -52,39 +53,41 @@ class Command(BaseCommand):
                 new_files.append(pdf_file)
         return sorted(new_files)
 
+
+
+
     def handle(self, *args, **options):
         """ Finds pdf files on disks and creates PrintIssue objects. """
-        log = self.stdout.write
-        # log = logger.debug
-
         for code, suffix in (1, ''), (2, '_mag'):
-            year, number = current_issue()
             filename = FILENAME_PATTERN.format(
-                year=year,
-                number=number,
+                issue=current_issue(),
                 suffix=suffix,
             )
             files = self.get_staging_pdf_files(code)
 
             if len(files) == 0:
-                log('no files found, %s' % code)
+                logger.info('no files found, %s' % code)
                 continue
 
             if len(files) % 4:
-                log('Incorrect number of pages (%d), %s' % (len(files), code))
+                logger.info('Incorrect number of pages (%d), %s' % (len(files), code))
                 continue
 
             pdf_path = os.path.join(PDF_FOLDER, filename)
 
-            log('\n'.join([PDF_MERGE, pdf_path] + files))
+            args = [PDF_MERGE, pdf_path] + files
+            logger.debug('\n'.join(args))
+            subprocess.call(args)
 
             try:
                 issue = PrintIssue.objects.get(pdf__endswith=filename)
             except PrintIssue.DoesNotExist:
                 issue = PrintIssue()
 
-            # issue, _ = PrintIssue.objects.get_or_create(pdf='pdf/' + filename)
-            name = '{number}/{year}{suffix}'.format(**locals())
+            name = '{issue.number}/{issue.date.year}{suffix}'.format(
+                    suffix = suffix, issue = current_issue())
+            with open(pdf_path, 'rb') as src:
+                content = ContentFile(src.read())
+            issue.pdf.save(filename, content, save=False)
             issue.issue_name = name
-            issue.pdf.save(pdf_path, save=False)
             issue.save()
