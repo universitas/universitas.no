847b22b83c0a2013be48f4566b53f66d57d9feb6
Håken Lid <haakenlid@gmail.com>
2016-09-04 13:09:02 +0200

    ny redaksjon

--- django/apps/contributors/context_processors.py
+++ django/apps/contributors/context_processors.py
@@ -5,12 +5,18 @@
 def staff(request):
     context = {
         'staff': [
-            {'position': 'Redaktør', 'name': 'Magnus Newth', 'email': 'mgnewth@universitas.no', },
-            {'position': 'Nettredaktør', 'name': 'Magnus Braaten', 'email': 'magnus.braaten@gmail.com', },
-            {'position': 'Daglig leder', 'name': 'Louise Faldalen Prytz', 'email': 'l.f.prytz@universitas.no', },
-            {'position': 'Redaksjonsleder', 'name': 'Julie Kalager', 'email': 'julika@universitas.no', },
-            {'position': 'Annonseselger', 'name': 'Geir Dorp', 'email': 'geir.dorp@universitas.no', },
-            {'position': 'Webutvikler', 'name': 'Håken Lid', 'email': 'haakenlid@gmail.com', },
+            {'position': 'Redaktør', 'name': 'Torgeir Mortensen',
+                'email': 'torgeirm3b@gmail.com', },
+            {'position': 'Nettredaktør', 'name': 'Sondre Moen Myhre',
+                'email': 'sondre.myhre1@gmail.com', },
+            {'position': 'Nyhetsredaktør', 'name': 'Birk Tjeldflaat Helle',
+                'email': 'birktjeldflaathelle@gmail.com', },
+            {'position': 'Daglig leder', 'name': 'Louise Faldalen Prytz',
+                'email': 'l.f.prytz@universitas.no', },
+            {'position': 'Annonseselger', 'name': 'Geir Dorp',
+                'email': 'geir.dorp@universitas.no', },
+            {'position': 'Webutvikler', 'name': 'Håken Lid',
+                'email': 'haakenlid@gmail.com', },
         ]
     }
     return context
