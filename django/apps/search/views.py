7e9c7129fad0fd2da65d0b4e37e5fd90973048bb
Håken Lid <haakenlid@gmail.com>
2016-09-06 12:02:16 +0200

    fix watson regression after update

--- django/apps/search/views.py
+++ django/apps/search/views.py
@@ -17,28 +17,9 @@ class SearchMixin:
     context_object_name = "search_results"
     query_param = "q"
 
-    # def get_query_param(self):
-        # """Returns the query parameter to use in
-        # the request GET dictionary."""
-    #     return self.query_param
-
-    # models = ()
-
-    # def get_models(self):
-    #     """Returns the models to use in the query."""
-    #     return self.models
-
-    # exclude = ()
-
-    # def get_exclude(self):
-    #     """Returns the models to exclude from the query."""
-    #     return self.exclude
-
     def get_queryset(self):
         """Returns the initial queryset."""
-        # return watson.search(self.query, models=self.get_models(),
-        # exclude=self.get_exclude())
-        search_results = watson.search(self.query)
+        search_results = watson.search.search(self.query)
         if search_results:
             search_results = search_results.prefetch_related("object")
 
@@ -46,7 +27,6 @@ class SearchMixin:
 
     def get_query(self, request):
         """Parses the query from the request."""
-        # return request.GET.get(self.get_query_param(), "").strip()
         return request.GET.get(self.query_param, "").strip()
 
     empty_query_redirect = None
@@ -55,25 +35,11 @@ class SearchMixin:
         """Returns the URL to redirect an empty query to, or None."""
         return self.empty_query_redirect
 
-    # extra_context = {}
-
-    # def get_extra_context(self):
-    #     """
-    #     Returns any extra context variables.
-
-    #     Required for backwards compatibility with old function-based views.
-    #     """
-    #     return self.extra_context
 
     def get_context_data(self, **kwargs):
         """Generates context variables."""
         context = super(SearchMixin, self).get_context_data(**kwargs)
         context["query"] = self.query
-    # Process extra context.
-    #     for key, value in six.iteritems(self.get_extra_context()):
-    #         if callable(value):
-    #             value = value()
-    #         context[key] = value
         return context
 
     def get(self, request, *args, **kwargs):
