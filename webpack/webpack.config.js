f2c1ad5171eba998342730c170720f59ad1394cc
Håken Lid <haakenlid@gmail.com>
2018-05-03 17:38:36 +0200

    comment webpack plugin

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -5,16 +5,26 @@ const build_dir = process.env.BUILD_DIR || '../build'
 
 module.exports = {
   entry: {
+    // entrypoints
+    // foundation based stylesheets
     stylesheets: 'stylesheets/universitas.scss',
+    // public page chunks
     head: 'javascripts/head.js',
     vendor: 'javascripts/vendor.js',
     foot: 'javascripts/foot.js',
+    // react apps
     photo_list_view: 'entrypoints/photo_list_view.js',
     photo_crop_app: 'entrypoints/photo_crop_app.js',
     prodsys: 'entrypoints/prodsys.js',
   },
+  output: {
+    // for example ../build/head.[hash].js
+    path: path.join(build_dir, ''),
+    filename: '[name].js',
+  },
   plugins: [
     new webpack.ProvidePlugin({
+      // implicitly `import`
       $: 'jquery',
       jQuery: 'jquery',
       React: 'react',
@@ -22,15 +32,18 @@ module.exports = {
       PropTypes: 'prop-types',
     }),
     new BundleTracker({
+      // for django integration
       indent: ' ',
       path: build_dir,
       filename: 'webpack-stats.json',
     }),
     new webpack.optimize.CommonsChunkPlugin({
+      // output a react-common chunk
       name: 'react-common',
       chunks: ['prodsys', 'photo_list_view', 'photo_crop_app'],
     }),
     new webpack.optimize.CommonsChunkPlugin({
+      // output a chunk to bootstrap react
       name: 'webpack-bootstrap',
       minChunks: Infinity,
     }),
@@ -44,7 +57,7 @@ module.exports = {
             loader: 'url-loader',
             options: {
               name: 'assets/[name]-[hash:12].[ext]',
-              limit: 20000,
+              limit: 20000, // inline smaller files in css
             },
           },
         ],
@@ -55,7 +68,7 @@ module.exports = {
           { loader: 'style-loader' },
           { loader: 'css-loader', options: { sourceMap: false } },
           {
-            loader: 'postcss-loader',
+            loader: 'postcss-loader', // for autoprefix
             options: {
               sourceMap: false,
               plugins: () => [require('autoprefixer')],
@@ -95,9 +108,5 @@ module.exports = {
       // http://stackoverflow.com/a/28989476/1977847
       jquery: 'jquery/src/jquery',
     },
-  },
-  output: {
-    path: path.join(build_dir, ''),
-    filename: '[name].js',
   },
 }
